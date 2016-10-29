834600b4e616dff6ddb0aa288f26098c048e21b6
chenshiok <chenshiok5772@gmail.com>
2016-10-29 10:58:36 +0800

    添加一个函数

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,16 @@
 #include<stdlib.h>
 #include<stdio.h>
+int fun()
+{
+	printf("添加一个函数!\n");
+	return 0;
+}
 
 int main()
 {
-	printf("hello world!");
-	printf("大家儿童节快乐!");
+	printf("hello world!\n");
+	printf("大家儿童节快乐!\n");
+	fun();
 	return 0;
 
 }
